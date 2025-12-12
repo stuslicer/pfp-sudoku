0773e234415b9ecb23d4f744f6fa94e03d58f4f3
Stu Slicer <stu_slicer@yahoo.co.uk>
2025-12-12 10:44:19 +0000

    Initial commit

---
+++ build.gradle.kts
@@ -0,0 +1,38 @@
+plugins {
+    kotlin("jvm") version "2.2.10"
+    kotlin("plugin.serialization") version "2.2.10"
+
+}
+
+group = "com.seachange"
+version = "1.0-SNAPSHOT"
+
+val langChain4JVersion = "1.4.0"
+val ktorClientVersion = "2.3.7"
+
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+
+    // logging
+    implementation("io.github.microutils:kotlin-logging-jvm:3.0.5")
+    implementation("org.slf4j:slf4j-api:2.0.16")
+    implementation("ch.qos.logback:logback-classic:1.5.13")
+
+    testImplementation(kotlin("test"))
+    testImplementation("org.junit.jupiter:junit-jupiter:5.10.0")
+    testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:5.10.0")
+    testImplementation("com.willowtreeapps.assertk:assertk-jvm:0.26.1")
+    testImplementation("io.mockk:mockk:1.13.5")
+
+}
+
+tasks.test {
+    useJUnitPlatform()
+}
+kotlin {
+    jvmToolchain(21)
+}
